7e68ee30ef4240f7ad42495858ce2e19ef8d39ee
DavidQChuang <dc1770712@gmail.com>
2023-06-27 13:52:17 -0400

    Updated exec.sh to cd to script dir, added venv

--- exec.sh
+++ exec.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 if [ -f "user-vars.sh" ]; then
     source user-vars.sh
 else
@@ -9,6 +11,13 @@ fi
 if [[ -z "${python_cmd}" ]]
 then
     python_cmd="python3"
+fi
+
+if [[ ! -d "venv" ]]
+then
+    "$python_cmd" -m venv venv || echo "venv should be installed. Run 'pip install venv'."
+    source venv/bin/activate
+    pip install -r requirements.txt
 fi
 
 "$python_cmd" Trainer.py "$@"
