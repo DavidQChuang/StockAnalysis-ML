e59592072d15ce20d58a2a7691ad37ba7fce1cb9
David Chuang <dc1770712@gmail.com>
2023-11-02 19:31:59 -0400

    Fixed exec.sh using system pip instead of python3.10 pip

--- exec.sh
+++ exec.sh
@@ -10,14 +10,14 @@ fi
 
 if [[ -z "${python_cmd}" ]]
 then
-    python_cmd="python3"
+    python_cmd="python3.10"
 fi
 
 if [[ ! -d "venv" ]]
 then
     "$python_cmd" -m venv venv || echo "venv should be installed. Run 'pip install venv'."
     source venv/bin/activate
-    pip install -r requirements.txt
+    "$python_cmd" -m pip install -r requirements.txt
 fi
 
 "$python_cmd" Trainer.py "$@"
